abc70f0147f5520381b58fb5f0a3e53b373c227b
April Ablon <ablonapril@gmail.com>
2017-03-12 21:08:48 +1000

    Completed getElementByClassName function
    Created the function for getElementsByClassName, as required it uses
    element.childNodes and classList. Unable to use document.body, unless
    the spec is changed to search document.body, so only uses document at
    the moment.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+// each function created in underbar
+_.each = function(collection, iterator) {
+  if (Array.isArray(collection)) {
+    for (var i = 0; i < collection.length; i++) {
+      iterator(collection[i], i, collection);
+    }
+  } else if (typeof collection === 'object') {
+    for (var key in collection) {
+      iterator(collection[key], key, collection);
+    }
+  }
+};
+
+var getElementsByClassName = function(className) {
+  var results = [];
+  function getClass(element) {
+    var collection = element.childNodes;
+    _.each(collection, function(node) {
+      if (node.classList && node.classList.contains(className)) {
+        results.push(node);
+      }
+      getClass(node);
+    })
+  }
+  getClass(document);
+  return results;
 };
