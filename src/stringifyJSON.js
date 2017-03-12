b8a6be88ca6d9263c406bbc75d2cc6c23b9273d8
April Ablon <ablonapril@gmail.com>
2017-03-12 20:18:59 +1000

    Completed stringifyJSON - passed all tests
    Function created passes all tests given in fixtures. Now works for
    nested arrays, objects, and nested objects.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -28,26 +28,48 @@ var stringifyJSON = function(obj) {
     for (var i = 0; i < obj.length - 1; i++) {
       if (typeof obj[i] === 'string') {
         string += '"' + obj[i].toString() + '",';
-      } else {
+      } else if (typeof obj[i] === 'number' || typeof obj[i] === 'boolean') {
         string += obj[i].toString() + ',';
+      } else if (Array.isArray(obj[i])) {
+        string += stringifyJSON(obj[i]) + ',';
+      } else if (typeof obj[i] === 'object' && !(Array.isArray(obj[i]))) {
+        string += stringifyJSON(obj[i]) + ',';
       }
     }
     var last = obj[obj.length - 1];
     if (typeof last === 'string') {
       string += '"' + last + '"]'
-    } else {
+    } else if (typeof last === 'number' || typeof last === 'boolean') {
       string += last.toString() + ']';
+    } else if (Array.isArray(obj[i])) {
+      string += stringifyJSON(obj[i]) + ']';
+    } else if (typeof obj[i] === 'object' && !(Array.isArray(obj[i]))) {
+      string += stringifyJSON(obj[i]) + ']';
     }
     return string;
     }
-  } else {
+  }
+  // for object inputs
+  if (typeof obj === 'object' && !(Array.isArray(obj))) {
     var string = '{';
+    var properties = [];
     for (var prop in obj) {
-      if (typeof obj[prop] !== 'object') {
-        str += prop.toString + ':' + obj[prop].toString + ', ';
-      } else {
-        return stringifyJSON(obj[prop]);
+      var key = '';
+      key += '"' + prop.toString() + '":';
+      if (obj[prop] === null || obj[prop] === undefined) {
+        key += 'null';
+      } else if (typeof obj[prop] !== 'object' && (typeof obj[prop] === 'number' || typeof obj[prop] === 'boolean')) {
+        key += obj[prop].toString();
+      } else if (typeof obj[prop] !== 'object' && typeof obj[prop] === 'string') {
+        key += '"' + obj[prop].toString() + '"';
+      } else if (typeof obj[prop] === 'object') {
+        key += stringifyJSON(obj[prop]);
+      }
+      if (typeof obj[prop] !== 'function' && obj[prop] !== undefined) {
+        properties.push(key);
       }
     }
+    string += properties.join(',') + '}';
+    return string;
   }
 };
