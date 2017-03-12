cdeb7fa4f7979eb355c9e704ce2c05a77178c960
April Ablon <ablonapril@gmail.com>
2017-03-12 19:23:40 +1000

    Working for null, number, boolean, string, array
    Added to the stringifyJSON function in order to get it to pass the
    first few test cases listed in fixtures. This included the following
    input types: null, number, boolean, string, and flat array.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // for null input
+  if (obj === null || obj === undefined) {
+    return 'null';
+  }
+  // for number or boolean inputs
+  if (typeof obj !== 'object' && (typeof obj === 'number' || typeof obj === 'boolean')) {
+    return obj.toString();
+  }
+  // for string inputs
+  if (typeof obj !== 'object' && typeof obj === 'string') {
+    var string = obj.split('');
+    string.push('"');
+    string.unshift('"');
+    return string.join('');
+  }
+  // for array inputs
+  if (typeof obj === 'object' && Array.isArray(obj)) {
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+    var string = '[';
+    for (var i = 0; i < obj.length - 1; i++) {
+      if (typeof obj[i] === 'string') {
+        string += '"' + obj[i].toString() + '",';
+      } else {
+        string += obj[i].toString() + ',';
+      }
+    }
+    var last = obj[obj.length - 1];
+    if (typeof last === 'string') {
+      string += '"' + last + '"]'
+    } else {
+      string += last.toString() + ']';
+    }
+    return string;
+    }
+  } else {
+    var string = '{';
+    for (var prop in obj) {
+      if (typeof obj[prop] !== 'object') {
+        str += prop.toString + ':' + obj[prop].toString + ', ';
+      } else {
+        return stringifyJSON(obj[prop]);
+      }
+    }
+  }
 };
